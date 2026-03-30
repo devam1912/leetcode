1a1e9547d2c8c3a3d3a2d67732c7eb599c8e5849
devam1912 <devamtanna07@gmail.com>
2026-03-30 11:11:42 +0530

    Time: 0 ms (100%), Space: 9 MB (0%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<=j)
+        {
+            if(s[i]==s[j]) return i;
+            i++;
+            j--;
+        }
+        return -1;
+    }
+};
