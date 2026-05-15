c4ae7e9e2abfcce4254a4e7f1d5d8f1284546277
devam1912 <devamtanna07@gmail.com>
2026-05-15 16:02:23 +0530

    Time: 844 ms (32.85%), Space: 187.9 MB (68.91%) - LeetHub

---
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minArraySum(vector<int>& nums) {
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        long long ans = 0;
+
+        for (int x : nums) {
+
+            int best = x;
+
+            for (int d = 1; d * d <= x; d++) {
+
+                if (x % d == 0) {
+
+                    if (st.count(d))
+                        best = min(best, d);
+
+                    int other = x / d;
+
+                    if (st.count(other))
+                        best = min(best, other);
+                }
+            }
+
+            ans += best;
+        }
+
+        return ans;
+    }
+};
