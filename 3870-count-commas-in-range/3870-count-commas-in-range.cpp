9898cc38c6973656a0e31bd407da165790fccccb
devam1912 <devamtanna07@gmail.com>
2026-09-08 10:25:37 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.47%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        
+       if(n<1000) return 0;
+       if(n>=1000 && n<10000) return ((n-1000)+1);
+       if(n>=10000 && n<=100000) return ((n-1000)+1);
+       return 0; 
+    }
+};
